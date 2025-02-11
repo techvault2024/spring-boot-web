7f84b53f184a94ec797ab4119cc808456d1b2aab
john <john@doe.com>
2025-02-11 07:06:41 -0500

    updated springBootTest

--- src/test/java/com/example/springbootweb/SpringBootWebApplicationTests.java
+++ src/test/java/com/example/springbootweb/SpringBootWebApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.springbootweb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringBootWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(applicationContext, "The application context should have loaded.");
+    }
 
 }
